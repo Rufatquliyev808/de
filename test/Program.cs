36b45b39aa5283dc5f7b77925c5fbc414bca437a
rufat.quliyev <rufat.quliyev@RUFATQULIYEV>
2025-02-08 20:52:05 +0400

    Add project files.

---
+++ test/Program.cs
@@ -0,0 +1,174 @@
+﻿//////// Tapsiriq 1
+
+//int a = 808;
+//string b = "Rufat";
+//char c = 'S';
+//bool d = true;
+
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+//Console.WriteLine(c);
+//Console.WriteLine(d);
+
+
+/////// Tapsiriq 2
+
+//Console.WriteLine("Adinizi Yazin!");
+//string name = Console.ReadLine();
+//Console.WriteLine("Soy adinizi yazin");
+//string surname = Console.ReadLine();
+//Console.Write("Sizin Ad ve Soy adiniz:" );
+//Console.WriteLine(name + " " + surname); 
+
+////////// Tapsiriq 3 
+
+//Console.WriteLine("Birinci reqemi daxil et");
+//int a=Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Ikinci reqemi daxil et");
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Ucuncu reqemi daxil et");
+//int c = Convert.ToInt32(Console.ReadLine());
+//Console.Write($"Enboyuk reqem:" + " ");
+
+//if (a > b)
+//{
+//    if (a > c) Console.WriteLine(a);
+//    else
+//    {
+//        if (c > b) Console.WriteLine(c);
+//    }
+//}
+//else
+//{
+//    if (b > c) Console.WriteLine(b);
+//    else
+//    {
+//        if (c > b) Console.WriteLine(c);
+//    }
+//}
+
+
+
+//////// Tapsiriq 4 mentiqi tutmadim
+
+//int b = 30;
+//int c = 10;
+
+//Console.WriteLine(b + c);
+//Console.WriteLine(b - c);
+//Console.WriteLine(b / c);
+//Console.WriteLine(b * c);
+
+// Sade Riyazi meseleler
+
+////////////////////////////////////////////
+
+//Console.WriteLine("Yasinizi daxil edin: ");
+//int age;
+//age = Convert.ToInt32(Console.ReadLine()); //sring i int tipine conver edir
+//Console.WriteLine($"Sizin yeni yasiniz: {age + 15} "); //age deyeri terminaldan isteyir ve +15 edir
+
+////////////////////////////////////////////
+
+
+//Console.WriteLine("Adinizi daxil edin: ");
+//string name = Console.ReadLine();
+//Console.WriteLine($"Sizin adiniz: {name}"); //$ Stringin icinde qarsilasmaq ucun lazimdir
+//Console.WriteLine("Xos Gelmisiniz");
+
+///////////////////////////////////////////
+
+//boolean true fals
+
+//Console.WriteLine("Yasinizi daxil edin");
+//int age;
+
+//age = Convert.ToInt32(Console.ReadLine());
+
+//bool isAllowed = age >= 18; 
+
+//if (isAllowed)
+//{
+//    Console.WriteLine("Istifade ede bilersiniz");
+//}
+//else
+//{
+//    Console.WriteLine("Olmaz!! ");
+//}
+
+
+/////////////////////////////////////////
+
+
+//int examPoint;
+
+//Console.WriteLine("Imtahan balinizi yazin: ");
+//examPoint = Convert.ToInt32(Console.ReadLine());
+
+//if (examPoint >= 50) //sertde (;) istifade etmirik
+//{
+//    Console.WriteLine("Siz imtahandan kecdiniz");
+
+//}
+
+//else
+//{
+//    Console.WriteLine("Siz Imtahandan kecmediniz! ");
+//}
+
+//////////////////////////////////////////////
+
+//int excemPoint;
+//Console.WriteLine("Imtahan balinizi qeyd edin: ");
+//excemPoint = Convert.ToInt32(Console.ReadLine());
+
+//if(excemPoint >=50 )
+//{
+//    Console.WriteLine("Siz Imtahandan Kecdiniz!:");
+//}
+//else if(excemPoint >= 30)
+//{
+//    Console.WriteLine("Kesilmisiniz amma ikinci sansiniz var:");
+//}
+//else
+//{
+//    Console.WriteLine("Siz Kesilmisiniz! ");
+//}
+
+/////////////////////////////////////////////////
+
+//switch (int. string ola biler)
+
+//switch (50) (deyer)
+//{
+//    case a1: bunu yoxlayir if 50 ye beraberdir yayox
+//    case a2: sonra bunu yoxlayir else if a2 deyerine beraberdir ya yox
+//    case a3 : ve s
+//   default: en son nedirese o serti yerine yetirir
+//}
+
+//int age = Convert.ToInt32(Console.ReadLine());
+//switch (age)
+//{
+//    case 1:
+//        Console.WriteLine("Bazar ertesi"); break;
+//    case 2:
+//        Console.WriteLine("Cersenbe Axsami"); break;
+//    case 3:
+//        Console.WriteLine("Cersenbe"); break;
+//    case 4:
+//        Console.WriteLine("Cume Axsami");break;
+//    case 5:
+//        Console.WriteLine("Cume");break ;
+//    case 6:
+//        Console.WriteLine("Senbe gunu");break;
+//    case 7:
+//        Console.WriteLine("Bazar gunu");break;
+//    default:
+//        Console.WriteLine("Be yas heddi bilinmir"); break;
+
+//}
+
+///////////////////////////////////////////////
+
+
